3dd7584fed969945e648b4c75d71eaad5a591074
Kim Serradell <kim.serradell@bsc.es>
2017-06-19 14:49:43 +0200

    First functional version of oifs package

--- var/spack/repos/builtin/packages/oifs/package.py
+++ var/spack/repos/builtin/packages/oifs/package.py
@@ -23,6 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import os
 
 
 class Oifs(Package):
@@ -40,19 +41,57 @@ class Oifs(Package):
     depends_on('grib-api')
     depends_on('openblas')
 
-    def setup_environment(self, spack_env, run_env):
-        run_env.set('OIFS_COM', 'gnu')
-        run_env.set('OIFS_BUILD', 'opt')
-
-    def edit(self, spec, prefix):
-        run_env.prepend_path('PATH', os.getcwd() + '/fcm/bin')
-        # Modify arch file
-        # add grib-api path 
-        # add openblas path
-    
     def install(self, spec, prefix):
+
+    	# Defining installation
+    	os.environ['OIFS_COM'] = 'spack'
+    	os.environ['OIFS_BUILD'] = 'opt'
+
+        # Clean previous installation
+        os.system('rm -rf ' + os.getcwd() + '/make/gnu-opt/') 
+        os.system('rm -rf ' + os.getcwd() + '/make/spack-opt/')
+        os.system('rm -rf ' + os.getcwd() + '/make/cfg/spack-opt.cfg')
+        os.system('rm -rf ' + os.getcwd() + '/make/cfg/gnu-opt.cfg')
+
+        # Starting with gnu compilation
+        with open(os.getcwd() + '/make/cfg/gnu-opt.cfg', 'w') as f:
+            f.writelines([
+				'$OIFS_GRIB_API_DIR{?}     = ' +  spec['grib-api'].prefix + '\n'
+				'$OIFS_GRIB_API_INCLUDE{?} = -I $OIFS_GRIB_API_DIR/include\n'
+				'$OIFS_GRIB_API_LIB{?}     = -L$OIFS_GRIB_API_DIR/lib -lgrib_api_f90 -lgrib_api\n'
+				'\n'
+				'# LAPACK & BLAS libraries\n'
+				'$LAPACK_LIB_DEFAULT = ' +  spec['openblas'].prefix.lib + ' -lopenblas \n' # -llapack -lblas\n'
+				'\n'					
+				'# Extra libraries (architecture/compiler specific)\n'
+				'$OIFS_EXTRA_LIB{?}  = \n'
+				'\n'
+				'# Source files that FCM should specifically ignore\n'
+				'$SRC_EXCL = \n'
+				'\n'
+				'#  Fortran\n'
+				'$OIFS_FC{?}     = mpif90\n'
+				'$OIFS_FFLAGS{?} = -g -O2 -m64 -fconvert=big-endian -fopenmp\n'
+				'$OIFS_FFIXED{?} = -fdefault-real-8 -fdefault-double-8 -ffixed-line-length-132\n'
+				'$OIFS_FCDEFS{?} = BLAS LITTLE LINUX INTEGER_IS_INT F90 PARAL NONCRAYF\n'
+				'$OIFS_LFLAGS{?} = -fopenmp\n'
+				'\n'
+				'# C compiler\n'
+				'$OIFS_CC{?}     = mpicc\n'
+				'$OIFS_CFLAGS{?} = -g -O -m64\n'
+				'$OIFS_CCDEFS{?} = BLAS LITTLE LINUX INTEGER_IS_INT _ABI64\n'
+				'\n'		
+            ])
+
+        
+        # Change directory to make
+        os.chdir(os.getcwd() + '/make/')
+
         # Execute fcm command (fcm make -v -f oifs.cfg)
-        make()
+        os.system('../fcm/bin/fcm make -j 8 -v -f oifs.cfg')
+        
+        # Check master.exe binary exists
+
 
 
 
